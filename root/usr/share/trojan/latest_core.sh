dab1d12097075a7ac959de83037d69460b151c6e
frainzy1477 <49537471+frainzy1477@users.noreply.github.com>
2021-04-27 03:41:31 +0000

    Update latest_core.sh

--- root/usr/share/trojan/latest_core.sh
+++ root/usr/share/trojan/latest_core.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-new_version=`curl -L -k --retry 2 --connect-timeout 20 -o - https://api.github.com/repos/frainzy1477/trojan-go/releases/latest 2>/dev/null|grep -E 'tag_name' |grep -E 'v[0-9.]+' |awk -F ':'  '{
-print $2}' | sed 's/\"//g' | sed 's/\,//g' | sed 's/\ //g'`
+new_version=`curl -sL "https://github.com/frainzy1477/trojan-go/tags"| grep "/frainzy1477/trojan-go/releases/"| head -n 1| awk -F "/tag/" '{print $2}'| sed 's/\">//'`
 if [ "$?" -eq "0" ]; then
 	rm -rf /usr/share/trojan/new_core
 	if [ $new_version ]; then
